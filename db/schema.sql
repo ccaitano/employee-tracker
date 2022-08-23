86e6ab8eebc3d1f5e167cc261b5f32ec653c53c5
ccaitano <cheryl.caitano@gmail.com>
2022-08-22 19:14:57 -1000

    Build schema.sql file with department, emp_role, and employee tables

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+USE company_db;
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE emp_role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES emp_role(id)
+    ON DELETE CASCADE
+    -- NEED TO ADD IN MANAGER CAPABILITY
+);
