006d554a7c149677c098f6b95c670bed84aba622
ccaitano <cheryl.caitano@gmail.com>
2022-08-22 19:29:05 -1000

    Build seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (department_name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales"),
+       ("Service");
+
+INSERT INTO emp_role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 4),
+       ("Salesperson", 80000, 4),
+       ("Lead Engineer", 150000, 1),
+       ("Software Engineer", 120000, 1),
+       ("Account Manager", 160000, 2),
+       ("Accountant", 125000, 2),
+       ("Legal Team Lead", 250000, 3),
+       ("Lawyer", 190000, 3),
+       ("Technician Lead", 90000, 5),
+       ("Technician", 60000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Smith", 2);
